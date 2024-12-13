26732efd1de0788c4f61a685f1dfa0eb720e6877
FREECS33 <20213tn116@utez.edu.mx>
2024-12-13 00:14:25 -0600

    [UPDATE] updated service for update form

--- src/main/java/mx/edu/utez/sigede_backend/controllers/user_info/dto/UserInfoUpdateDTO.java
+++ src/main/java/mx/edu/utez/sigede_backend/controllers/user_info/dto/UserInfoUpdateDTO.java
@@ -1,8 +1,8 @@
 package mx.edu.utez.sigede_backend.controllers.user_info.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Data
@@ -10,11 +10,13 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class UserInfoUpdateDTO {
     private Long fieldId;
-    @Getter
-    private Boolean isRequired;
+    @JsonProperty("isRequired")
+    private boolean isRequired;
     private String tag;
     private String type;
+    @JsonProperty("isInQr")
     private boolean isInQr;
+    @JsonProperty("isInCard")
     private boolean isInCard;
 
 }
